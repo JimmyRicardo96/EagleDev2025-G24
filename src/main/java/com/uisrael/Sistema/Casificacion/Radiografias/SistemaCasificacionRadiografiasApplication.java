4c3d9eb5aa4d6dd59e13c385f8f917739a9f6dc9
Jimmy <e1725189557@uisrael.edu.ec>
2025-05-31 18:09:50 -0500

    Inicial Proyecto

---
+++ src/main/java/com/uisrael/Sistema/Casificacion/Radiografias/SistemaCasificacionRadiografiasApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.Sistema.Casificacion.Radiografias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCasificacionRadiografiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCasificacionRadiografiasApplication.class, args);
+	}
+
+}
