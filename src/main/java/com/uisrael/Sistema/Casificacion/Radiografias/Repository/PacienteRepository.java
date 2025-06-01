e5f08eab220e535b649ad4d36ee355e2892417f8
Jimmy <e1725189557@uisrael.edu.ec>
2025-05-31 19:38:51 -0500

    Paciente, entity, controller, service

--- src/main/java/com/uisrael/Sistema/Casificacion/Radiografias/Repository/PacienteRepository.java
+++ src/main/java/com/uisrael/Sistema/Casificacion/Radiografias/Repository/PacienteRepository.java
@@ -1,4 +1,12 @@
 package com.uisrael.Sistema.Casificacion.Radiografias.Repository;
 
-public interface PacienteRepository {
+import com.uisrael.Sistema.Casificacion.Radiografias.Entity.PacienteEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<PacienteEntity, Integer> {
+Optional<PacienteEntity> findByIdentificacion(String identificacion);
 }
